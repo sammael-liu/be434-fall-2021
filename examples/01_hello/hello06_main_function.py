379646d11dcdfc09800b89a1e8ad3c79eaf1e788
Wen-Wei Liu <wliu@Wen-Weis-MacBook-Air.local>
2021-08-31 21:49:27 -0700

    change 0831

--- examples/01_hello/hello06_main_function.py
+++ examples/01_hello/hello06_main_function.py
@@ -10,5 +10,10 @@ def main():
     args = parser.parse_args()
     print('Hello, ' + args.name + '!')
 
-if __name__ == '__main__':
+
+if __name__ == '__main__': 
     main()
+#you can change the main(), ex. run(), but __main__ will still be the same
+
+#use main() instead of line 13 and 14 can also work in the current situation,
+#but it is better to have line 13, 14 when using this script as library.
