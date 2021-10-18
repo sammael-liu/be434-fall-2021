b7e75e1231fec2b65923d47c6ef56260fa7ed06f
Wen-Wei Liu <wliu@Wen-Weis-MacBook-Air.local>
2021-10-17 20:26:45 -0700

    new file

---
+++ assignments/07_iupac/iupac.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""
+Author : wliu <wliu@localhost>
+Date   : 2021-10-17
+Purpose: Expand IUPAC codes
+"""
+
+import argparse
+import sys
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Expand IUPAC codes',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('seq',
+                        help='Input sequence(s)',
+                        metavar='SEQ',
+                        type=str,
+                        nargs='+')
+
+    parser.add_argument('-o',
+                        '--outfile',
+                        help='Output filename',
+                        metavar='FILE',
+                        type=argparse.FileType('wt'),
+                        default=sys.stdout)
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    iupac = {
+        'A': 'A',
+        'C': 'C',
+        'G': 'G',
+        'T': 'T',
+        'U': 'U',
+        'R': '[AG]',
+        'Y': '[CT]',
+        'S': '[GC]',
+        'W': '[AT]',
+        'K': '[GT]',
+        'M': '[AC]',
+        'B': '[CGT]',
+        'D': '[AGT]',
+        'H': '[ACT]',
+        'V': '[ACG]',
+        'N': '[ACGT]'
+    }
+
+    for seqs in args.seq:
+        new = ''
+        for chars in seqs:
+            new += iupac.get(chars)
+        print('{} {}'.format(seqs, new), file=args.outfile)
+    if args.outfile != sys.stdout:
+        print(f'Done, see output in "{args.outfile.name}"')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
